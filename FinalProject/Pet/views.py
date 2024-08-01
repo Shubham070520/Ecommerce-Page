bccf4a2925a77a1fcd9a0733ecc3960e1319fdb1
Shubham Rane <shubhamrane016@gmail.com>
2024-08-01 11:12:27 +0530

    Setting up the environment and Forming Templates

--- FinalProject/Pet/views.py
+++ FinalProject/Pet/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import HttpResponse
 from django.shortcuts import render,redirect
  
 
-# Create your views here.
+# # Create your views here.
+# def home(request):
+#     return render(request,'base.html')
+
 def home(request):
-    return render(request,'index.html')
+    return render(request,'index2.html')
